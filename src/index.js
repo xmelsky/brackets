03807e935094d07816a7ca5d555c52440c21fd35
dima xmiel <dima.hmelios@gmail.com>
2020-03-03 09:05:10 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const res = [];
+    for (const char of str) {
+        if (res[res.length - 1] == char) {
+            res.pop();
+        } else {
+            const pair = bracketsConfig.find(pair => pair[0] === char);
+            if (pair) res.push(pair[1]);
+            else return false;
+        }
+    }
+    return !res.length;
+};
